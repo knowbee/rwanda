1a73af80555c709b81ec3b255d51eedc8e039674
Googler <knowbeeinc@gmail.com>
2019-11-30 13:36:19 +0200

    display districts from a single province and all sectors

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-const {datafile} = require("./data")
-
+const { datafile } = require("./data");
 
 const formatInput = name => {
-  return name.charAt(0).toUpperCase() + name.substring(1)
-}
+  return name.charAt(0).toUpperCase() + name.substring(1);
+};
 
 exports.Provinces = () => {
-  return Object.keys(datafile)
-}
+  return Object.keys(datafile);
+};
 
 exports.Districts = () => {
-  let districts = []
+  let districts = [];
   for (let province of Object.keys(datafile)) {
     for (let district of Object.keys(datafile[province])) {
-      districts.push(district)
+      districts.push(district);
     }
   }
-  return districts
-}
+  return districts;
+};
+
+(exports.District = province => {
+  if (Object.keys(datafile).includes(formatInput(province))) {
+    return Object.keys(datafile[formatInput(province)]);
+  } else {
+    return undefined;
+  }
+}),
+  (exports.Sectors = () => {
+    let sectors = [];
+    for (let province of Object.keys(datafile)) {
+      for (let district of Object.keys(datafile[province])) {
+        for (let sector of Object.keys(datafile[province][district])) {
+          sectors.push(sector);
+        }
+      }
+    }
+    return sectors;
+  });
