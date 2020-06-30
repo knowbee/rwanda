34e3ae7dc57c36718d52065ccb33f6c0c44f2b3e
knowbee <knowbeeinc@gmail.com>
2020-06-30 20:38:05 +0200

    version 2.0.9

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 const datafile = require("./data/provinces");
 
 const formatInput = (name) => {
-  if (name && name.includes(" ")) {
-    newName = name.split(" ");
-    return `${newName[0].charAt(0).toUpperCase()}${newName[0]
-      .substring(1)
-      .toLowerCase()} ${newName[1]}`;
-  } else {
-    return name && name.length > 2
-      ? `${name.charAt(0).toUpperCase()}${name.substring(1).toLowerCase()}`
-      : undefined;
-  }
+  return name.replace(/^./, name[0].toUpperCase());
 };
 /**
  * Provinces: it returns all provinces found in Rwanda
